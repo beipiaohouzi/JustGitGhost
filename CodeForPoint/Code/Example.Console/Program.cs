29dbfbc38eda01c4a5757460f33d98530c8bc390
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-08-16 00:09:46 +0800

    练习 1 sqlite+ef

---
+++ CodeForPoint/Code/Example.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyncDataHelper.SyncCategory();
+        }
+    }
+}
---
+++ CodeForPoint/Code/Example.Console/UinDataService.SQLite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+namespace Example.Console
+{
+    
+    public class SyncDataHelper 
+    {
+        public static void SyncCategory() 
+        {
+            try
+            {
+                List<CategoryData> list = new List<CategoryData>();
+                for (int i = 0; i < 30; i++)
+                {
+                    CategoryData c = new CategoryData()
+                    {
+                        Code = "C" + i,
+                        Name = "N" + i,
+                        CreateTime = DateTime.Now,
+                        Id = i,
+                        IsDelete = false,
+                        ItemType = "City",
+                        NodeLevel = 0,
+                        ParentCode = "-1",
+                        ParentId = -1,
+                        Sort = 1
+                    };
+                }
+                SQLiteReporistory<CategoryData> md = new SQLiteReporistory<CategoryData>("data source=LocalDB.db;");
+                md.BatchAdd(list);
+            }
+            catch (Exception ex)
+            { 
+            
+            }
+        }
+    }
+  
+}
