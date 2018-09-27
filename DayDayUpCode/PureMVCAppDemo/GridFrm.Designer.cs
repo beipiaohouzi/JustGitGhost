7e8aa430879f230cb57c1f9f9183803fd2c9b975
158055983@qq.com <158055983@qq.com>
2018-09-27 15:30:20 +0800

    漏文件

---
+++ DayDayUpCode/PureMVCAppDemo/GridFrm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PureMVCAppDemo
+{
+    partial class GridFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xpPanel1 = new XPect.Lib.XPControl.XPPanel();
+            this.btnDeleteRow = new System.Windows.Forms.Button();
+            this.btnAddRow = new System.Windows.Forms.Button();
+            this.xpPanel2 = new XPect.Lib.XPControl.XPPanel();
+            this.tablePanel = new XPect.Lib.XPControl.XPTableLayoutPanel();
+            this.btnAddCol = new System.Windows.Forms.Button();
+            this.btnDeleteCol = new System.Windows.Forms.Button();
+            this.xpPanel1.SuspendLayout();
+            this.xpPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // xpPanel1
+            // 
+            this.xpPanel1.Controls.Add(this.btnDeleteCol);
+            this.xpPanel1.Controls.Add(this.btnAddCol);
+            this.xpPanel1.Controls.Add(this.btnDeleteRow);
+            this.xpPanel1.Controls.Add(this.btnAddRow);
+            this.xpPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.xpPanel1.Location = new System.Drawing.Point(0, 0);
+            this.xpPanel1.Name = "xpPanel1";
+            this.xpPanel1.Size = new System.Drawing.Size(680, 33);
+            this.xpPanel1.TabIndex = 7;
+            // 
+            // btnDeleteRow
+            // 
+            this.btnDeleteRow.Location = new System.Drawing.Point(370, 7);
+            this.btnDeleteRow.Name = "btnDeleteRow";
+            this.btnDeleteRow.Size = new System.Drawing.Size(139, 23);
+            this.btnDeleteRow.TabIndex = 5;
+            this.btnDeleteRow.Text = "Delete Row";
+            this.btnDeleteRow.UseVisualStyleBackColor = true;
+            // 
+            // btnAddRow
+            // 
+            this.btnAddRow.Location = new System.Drawing.Point(191, 7);
+            this.btnAddRow.Name = "btnAddRow";
+            this.btnAddRow.Size = new System.Drawing.Size(149, 23);
+            this.btnAddRow.TabIndex = 4;
+            this.btnAddRow.Text = "Add Row";
+            this.btnAddRow.UseVisualStyleBackColor = true;
+            // 
+            // xpPanel2
+            // 
+            this.xpPanel2.Controls.Add(this.tablePanel);
+            this.xpPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xpPanel2.Location = new System.Drawing.Point(0, 33);
+            this.xpPanel2.Name = "xpPanel2";
+            this.xpPanel2.Size = new System.Drawing.Size(680, 524);
+            this.xpPanel2.TabIndex = 8;
+            // 
+            // tablePanel
+            // 
+            this.tablePanel.ColumnCount = 2;
+            this.tablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tablePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablePanel.Location = new System.Drawing.Point(0, 0);
+            this.tablePanel.Name = "tablePanel";
+            this.tablePanel.RowCount = 1;
+            this.tablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tablePanel.Size = new System.Drawing.Size(680, 524);
+            this.tablePanel.TabIndex = 0;
+            // 
+            // btnAddCol
+            // 
+            this.btnAddCol.Location = new System.Drawing.Point(12, 7);
+            this.btnAddCol.Name = "btnAddCol";
+            this.btnAddCol.Size = new System.Drawing.Size(149, 23);
+            this.btnAddCol.TabIndex = 6;
+            this.btnAddCol.Text = "Add col";
+            this.btnAddCol.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteCol
+            // 
+            this.btnDeleteCol.Location = new System.Drawing.Point(529, 7);
+            this.btnDeleteCol.Name = "btnDeleteCol";
+            this.btnDeleteCol.Size = new System.Drawing.Size(139, 23);
+            this.btnDeleteCol.TabIndex = 7;
+            this.btnDeleteCol.Text = "Delete Col";
+            this.btnDeleteCol.UseVisualStyleBackColor = true;
+            // 
+            // GridFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 557);
+            this.Controls.Add(this.xpPanel2);
+            this.Controls.Add(this.xpPanel1);
+            this.Name = "GridFrm";
+            this.Text = "GridFrm";
+            this.xpPanel1.ResumeLayout(false);
+            this.xpPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XPect.Lib.XPControl.XPPanel xpPanel1;
+        private System.Windows.Forms.Button btnDeleteRow;
+        private System.Windows.Forms.Button btnAddRow;
+        private XPect.Lib.XPControl.XPPanel xpPanel2;
+        private XPect.Lib.XPControl.XPTableLayoutPanel tablePanel;
+        private System.Windows.Forms.Button btnDeleteCol;
+        private System.Windows.Forms.Button btnAddCol;
+    }
+}
---
+++ DayDayUpCode/PureMVCAppDemo/GridFrm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XPect.Lib.XPControl;
+using PureMVC.Interfaces;
+namespace PureMVCAppDemo
+{
+    public partial class GridFrm : BasePureMVCMediator
+    {
+        int col = 0, rowHeight = 0;
+        int mColWidth = 0;
+        public GridFrm()
+        {
+            InitializeComponent();
+            InitEle();
+        }
+        private void InitEle()
+        {
+            btnAddRow.Click += new EventHandler(btnAddRow_Click);
+            btnDeleteRow.Click += new EventHandler(btnDeleteRow_Click);
+            btnAddCol.Click += new EventHandler(BtnAddCol_Click);
+            col = 3;
+            rowHeight = 260;
+            mColWidth = tablePanel.Width / col;
+            tablePanel.RowStyles.Clear();
+            AddGridRow(col, rowHeight);
+            tablePanel.AutoScroll = true;
+        }
+        private void AddGridRow(int col,int rowHeight)
+        { 
+            tablePanel.ColumnCount = col;
+            for (int i = 0; i < col; i++)
+            {
+                this.tablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, mColWidth));
+            }
+            this.tablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, rowHeight));
+            XPImagePanel img = new XPImagePanel()
+            {
+                Width = mColWidth,
+                Height = rowHeight
+            };
+            img.RefreshDesc("Index:" + tablePanel.Controls.Count);
+            tablePanel.Controls.Add(img);
+        }
+        private void BtnAddCol_Click(object sender,EventArgs e)
+        {
+            XPImagePanel img = new XPImagePanel()
+            {
+               Height= rowHeight,
+               Width=mColWidth
+            };
+            img.RefreshDesc("Index:" + tablePanel.Controls.Count);
+            tablePanel.Controls.Add(img);
+        }
+        private void btnAddRow_Click(object sender, EventArgs e)
+        {
+            AddGridRow(col,rowHeight);
+        }
+
+        private void btnDeleteRow_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region  overide
+        public override void HandleNotification(INotification notification)
+        {
+            switch (notification.Name)
+            {
+                case NotifyData.Cmd_Account:
+                    string msg = notification.Body as string;
+                    Console.WriteLine(string.Format("form :{0} receiver msg:{1}", this.GetType().Name, msg));
+                    break;
+            }
+        }
+        public override string[] ListNotificationInterests()
+        {
+            return new string[] {
+                NotifyData.Cmd_Grid,
+                NotifyData.Cmd_Account
+           };
+        }
+        #endregion 
+    }
+}
