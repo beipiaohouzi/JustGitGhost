7d0221057bcf7305618ad08389ec59614a1fb971
158055983@qq.com <158055983@qq.com>
2018-11-01 11:29:59 +0800

    实际上是进行图片截图

--- DayDayUpCode/PureMVCAppDemo/Frm/FrmMultipleIcon.cs
+++ DayDayUpCode/PureMVCAppDemo/Frm/FrmMultipleIcon.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PureMVCAppDemo
 {
     public partial class FrmMultipleIcon : Form
     {
+        bool loadImage = false;
         public FrmMultipleIcon()
         {
             InitializeComponent();
@@ -21,16 +23,25 @@ namespace PureMVCAppDemo
         {
             btnAdd.Click += new EventHandler(Click_Event);
             btnAdd.MouseLeave += new EventHandler(MouseLeave_Event);
-            PaintButton(false);
+            if (loadImage)
+                PaintButton(false);
+            else
+                DrawImage2Ele(false,btnAdd.Size);
            
         }
         private void Click_Event(object sender,EventArgs e)
         {
-            PaintButton(true);
+            if (loadImage)
+                PaintButton(true);
+            else
+                DrawImage2Ele(true,btnAdd.Size);
         }
         private void MouseLeave_Event(object sender, EventArgs e)
         {
-            PaintButton(false);
+            if (loadImage)
+                PaintButton(false);
+            else
+                DrawImage2Ele(false,btnAdd.Size);
         }
         private void PaintButton(bool isClick)
         {
@@ -40,6 +51,30 @@ namespace PureMVCAppDemo
             btnAdd.BackgroundImageLayout = ImageLayout.Zoom;
             btnAdd.Image = new Bitmap(icon);
            
+        }
+        private void DrawImage2Ele(bool isClick,Size container)
+        {
+            string icon = !isClick ? @"Resource\addprocedure_normal.png" : @"Resource\addprocedure_active.png";
+            string bg = !isClick ? @"Resource\commonBtn_normal.png" : @"Resource\commonBtn_active.png";
+            btnAdd.BackgroundImage = GraphsicImage(bg,container);
+            btnAdd.Image = GraphsicImage(icon, container);
+            btnAdd.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+        private Image GraphsicImage(string path,Size targetSize)
+        {//这个函数实际上是进行图片截图
+            //读取文件流
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            Image origin = new Bitmap(fs);
+            Image tar = new Bitmap(targetSize.Width, targetSize.Height);
+            Graphics g = Graphics.FromImage(tar);
+            Rectangle rect = new Rectangle(new Point(0, 0), targetSize);//应该是进行等比例缩放
+
+            Rectangle origRect = new Rectangle(new Point(0, 0), origin.Size);//原图位置（默认从原图中截取的图片大小等于目标图片的大小）
+            g.DrawImage(origin, rect, origRect, GraphicsUnit.Pixel);
+            //tar.Save(DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");//本地文件检测是否和显示文件为同一个
+            fs.Close();
+            origin.Dispose();
+            return tar;
         }
     }
 }
